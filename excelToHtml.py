beb818e61ab8571ab4b9ac9fdfa58a0d8bd0a3c9
senspond20 <senspond@gmail.com>
2021-03-05 08:59:24 +0900

    layout ouput

--- excelToHtml.py
+++ excelToHtml.py
@@ -7,14 +7,19 @@ df = pd.DataFrame(dataSet, columns=["일자","언론사", "제목","원본주소
 html = df.to_html(
         index=False, 
         classes="table", 
-        border="none", 
         justify="center")
 # print(html)
 
-f = open('layout.html','r')
+f = open('layout.html','r', encoding='utf-8')
 layout = f.read()
 f.close()
 
-print(layout)
+# print(layout)
 
+render = layout.replace("{{content}}", html)
+# print(render)
+
+f = open('output.html', 'w',encoding='utf-8')
+f.write(render)
+f.close()
 # print(fd)
