c2eaae49405afeb4eb183caabed343a83d686b06
senspond20 <senspond@gmail.com>
2021-03-04 18:21:04 +0900

    excelTohtml

--- excel.py
+++ excel.py
@@ -3,11 +3,3 @@ import pandas as pd
 dataSet = pd.read_excel('data/test.xlsx', sheet_name='Sheet1')
 
 df = pd.DataFrame(dataSet, columns=["일자","언론사", "제목","원본주소"])
-print(df)
-html = df.to_html(
-        index=False, 
-        classes="table", 
-        border="none", 
-        justify="center")
-print(html)
-# print(fd)
---
+++ excelToHtml.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+dataSet = pd.read_excel('data/test.xlsx', sheet_name='Sheet1')
+
+df = pd.DataFrame(dataSet, columns=["일자","언론사", "제목","원본주소"])
+# print(df)
+html = df.to_html(
+        index=False, 
+        classes="table", 
+        border="none", 
+        justify="center")
+# print(html)
+
+f = open('layout.html','r')
+layout = f.read()
+f.close()
+
+print(layout)
+
+# print(fd)
